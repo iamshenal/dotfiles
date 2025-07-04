fe53d6920f663e1a7fa10d9352d27302e52b9ed3
Shenal Sudasinghe <mssudasinghe@gmai.com>
2025-07-04 16:01:31 +0530

    Revert "ghostty config, little tweaks for nvim"
    This reverts commit 0b54d6d5dc62bea840355bcfe0ab313d7fa7729b.

--- nvim.bak/lua/config/keymaps.lua
+++
@@ -1,5 +0,0 @@
--- custom keymappings
-local set = vim.keymap.set
-
--- easy change inside word
-set("n", "<C-i>", "ciw")
--- nvim.bak/lua/plugins/disabled.lua
+++
@@ -1,6 +0,0 @@
-return {
-  { "echasnovski/mini.indentscope", enabled = false },
-  { "lukas-reineke/indent-blankline.nvim", enabled = false },
-  { "akinsho/bufferline.nvim", enabled = false },
-  { "folke/flash.nvim", enabled = false },
-}
