e1271a8a0b83e05ccd8091f7fd1bc26e68893b6a
Shenal Sudasinghe <mssudasinghe@gmai.com>
2025-07-09 16:23:29 +0530

    tweaks*

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -1,7 +1,7 @@
 -- some keymaps will be here
 local set = vim.keymap.set
-set("n", "{", ":bprev<CR>")
-set("n", "}", ":bnext<CR>")
+set("n", "<leader>[", ":bprev<CR>")
+set("n", "<leader>]", ":bnext<CR>")
 set("n", "<leader>bd", ":bdelete<CR>")
 set("n", "<leader>|", ":vsplit<CR>")
 set("n", "<C-->", ":split<CR>")
